b9567bf4b7c34913104886e9537cf5d5a3387053
grantkollar <89742140+grantkollar@users.noreply.github.com>
2026-04-24 09:14:17 -0400

    Add Ski controller and views for Index, Resorts, Gear, and Tips pages

--- FinalProject/Controllers/SkiController.cs
+++ FinalProject/Controllers/SkiController.cs
@@ -1,9 +1,15 @@
+using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProject.Controllers
 {
     public class SkiController : Controller
     {
+        // inject the db context so we can pull resort data
+        private SkiContext data;
+        public SkiController(SkiContext ctx) => data = ctx;
+
         public IActionResult Index()
         {
             return View();
@@ -11,10 +17,17 @@ namespace FinalProject.Controllers
 
         public IActionResult Resorts()
         {
-            return View();
+            // grab all resorts from the db and sort them alphabetically
+            var resorts = data.Resorts.OrderBy(r => r.Name).ToList();
+            return View(resorts);
         }
 
         public IActionResult Gear()
+        {
+            return View();
+        }
+
+        public IActionResult Tips()
         {
             return View();
         }
